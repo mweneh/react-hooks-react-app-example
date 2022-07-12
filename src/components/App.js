d87c128e8093c6e408facea4fc1b563793b8c0e5
David <davidongaroh@gmail.com>
2022-07-12 22:03:08 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       
-      <h1>Now</h1>
+      <h1> Now </h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
